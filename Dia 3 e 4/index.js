ff6c91d418f815093b678d875e74e88acf6ff059
Garcia019 <matheugarcia2010@gmail.com>
2023-09-12 11:36:33 -0300

    Atualização final dos tópicos dia 3 e 4.

--- Dia 3 e 4/index.js
+++ Dia 3 e 4/index.js
@@ -1,7 +1,26 @@
-let teste = "Hello World!"
-console.log("Hello World!")
-console.log(teste)
-let tipoTexto = "2";
+let teste = "Hello World!";
+console.log("Hello World!");
+console.log(teste);
+let tipoTexto = "2"; // TIpos de Variáveis
 let TipoNumber = 2;
 let TipoBoolTexto = "TRUE";
 let TipoBool = true;
+
+let numUm = 1;
+let numDois = 2;
+console.log(numUm + numDois);
+
+let entradaUsuario = Number(prompt("insira sua idade"))
+let ano = 2023
+let idadeUsuario = ano - entradaUsuario
+console.log("A idade do Usuário é de", idadeUsuario)
+
+let condicao = true; // Condicionais
+if (condicao){ 
+    console.log("emtrou no if")
+}else{
+    console.log("entrou no else")
+}
+
+console.log(2 === 2 && 3 === 3) // TRUE, operador E/AND
+console.log(2 === 2 || 3 === 4) // TRUE, operador OU/OR
