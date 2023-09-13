3a97709dda04e290c666ed07011d45da93f5063c
Garcia019 <matheugarcia2010@gmail.com>
2023-09-13 10:31:13 -0300

    Atualizações de aprendizado JS Dias 5 e 6

---
+++ Dia 5 e 6/desafio.js
@@ -0,0 +1,106 @@
+let dia = Number(prompt("Digite o Número correspondente ao dia a ser selecionado:\n1 - DIA 5 \n2 - DIA 6"))
+switch(dia){
+    case 1:
+        let desafio = Number(prompt("Digite o número correspondente ao desafio:\n 1 - Desafio 1\n 2 - Desafio 2"))
+        switch(desafio){
+            case 1:
+                alert("Calculadora de Dois Números")
+                let numeros = null
+                function solicitarDoisNumeros(){
+                    let numeroUm = Number(prompt("Digite o primeiro número:"))
+                    let numeroDois = Number(prompt("Digite o segundo número:"))
+                    return [numeroUm, numeroDois]
+                }
+                let operacao = Number(prompt("Digite o número correspondente a operação desejada:\n1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n 4 - DIVISÃO"))
+                switch(operacao){
+                    case 1:
+                        numeros = solicitarDoisNumeros()
+                        alert(`A soma dos dois números é: ${numeros[0] + numeros[1]} !`)
+                        console.log(`A Subtração dos dois números é: ${numeros[0] + numeros[1]} !`)
+                        break;
+                    case 2:
+                        numeros = solicitarDoisNumeros()
+                        alert(`A Subtração dos dois números é: ${numeros[0] - numeros[1]} !`)
+                        console.log(`A Subtração dos dois números é: ${numeros[0] - numeros[1]} !`)
+                        break;
+                    case 3:
+                        numeros = solicitarDoisNumeros()
+                        alert(`A Multiplicação dos dois números é: ${numeros[0] * numeros[1]} !`)
+                        console.log(`A Subtração dos dois números é: ${numeros[0] * numeros[1]} !`)
+                        break;
+                    case 4:
+                        numeros = solicitarDoisNumeros()
+                        alert(`A Divisão dos dois números é: ${numeros[0] / numeros[1]} !`)
+                        console.log(`A Subtração dos dois números é: ${numeros[0] / numeros[1]} !`)
+                        break;
+                    default:
+                        alert("Você não selecionou umas das 4 operações!")    
+                }
+                break;
+            case 2:
+                let opcoes = Number(prompt("Digite qual opção deseja escolher:\n1 - Abastecer com gasolina\n2 - Abastecer com álcool\n3 - Calibrar os pneus"))
+                let valor = null
+                switch(opcoes){
+                    case 1:
+                        valor = Number(prompt("Digite o valor que deseja abastecer de gasolina:"))
+                        alert(`Foram abastecidos ${valor/5} litros de gasolina.`)
+                        console.log(`Foram abastecidos ${valor/5} litros de gasolina.`)
+                        break;
+                    case 2:
+                        valor = Number(prompt("Digite o valor que deseja abastecer de álcool:"))
+                        alert(`Foram abastecidos ${valor/3} litros de álcool.`)
+                        console.log(`Foram abastecidos ${valor/3} litros de álcool.`)
+                        break;
+                    case 3:
+                        alert("Pneus calibrados com sucesso.")
+                        console.log("Pneus calibrados com sucesso.")
+                        break;
+                    default:
+                        alert("Você não selecionou nenhuma das 3 opções do posto.")
+                        break;
+                }
+                break;
+            default:
+                alert("Você não selecionou nenhum Desafio!")
+        }
+        break;
+    case 2:
+        let exercicio = Number(prompt("Digite o número correspondente ao Exercício:\n 1 - Exercício 1\n 2 - Exercício 2\n 3 - Exercício 3\n 4 - Exercício 4\n"))
+        switch(exercicio){
+            case 1:
+                let numeroFinal = Number(prompt("Digite o número final da contagem:"))
+                alert(`OK, usando o for para contar do 0 até o ${numeroFinal}!`)
+                for(let contador = 0; contador <= numeroFinal; contador++){
+                    console.log(contador)
+                }
+                break;
+            case 2:
+                alert(`OK, usando o for para contar do 0 até o 50 de 5 em 5 números!`)
+                for(let contador = 0; contador <= 50; contador += 5){
+                    console.log(contador)
+                }
+                break;
+            case 3:
+                alert(`OK, usando o for para contar do 50 até o 0 de 5 em 5 números!`)
+                for(let contador = 50; contador >=0; contador -=5){
+                    console.log(contador)
+                }
+                break;
+            case 4:
+                let numeroTabuada = Number(prompt("Digite o número que deseja iniciar na criação das 3 tabuadas:"))
+                for(let contador1 = numeroTabuada; contador1 < numeroTabuada+3; contador1++){
+                    console.log(`Segue a tabuada do Número ${contador1} !`)
+                    for(let contador = 0; contador <= 10; contador ++){
+                        console.log(`${contador1} X ${contador} = ${contador1*contador} !`)
+                    }
+                }
+                break;
+            default:
+                alert("Você não selecionou nenhum exercício proposto!")
+                break;
+        }
+        break;
+    default:
+        alert("Você não selecionou nenhum Dia proposto!")
+        break;
+}
